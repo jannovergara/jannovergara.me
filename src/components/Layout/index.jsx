104b436009711400e254a46cc0308b632c2fe543
Janno <Janno>
2019-04-22 11:40:24 +0800

    Updates

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -14,7 +14,6 @@ class Layout extends React.Component {
             { charSet: 'x-ua-compatible' },
             { httpEquiv: 'x-ua-compatible', content: 'ie=edge' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' },
-            { name: 'description', content: '#include<vector> unique_ptr<ClassName> make() const override for(auto&& t : list) return 0 virtual void prepare(int volume) = 0; ' },
             { name: 'keywords', content: 'Janno, Vergara, Janno Vergara, Physics, C++, Python, Javascript' },
             { name: 'google-site-verification', content: 'WYGvvZM3YJeHXNebajyieuAWbd0yoWsamZNkkhF5hAE' },
             { name: 'apple-mobile-web-app-title', content: 'https://www.jannovergara.me' },
